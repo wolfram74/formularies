c5aec4c9e419cb47307456146bfdf65ed4ce33f2
wolfram74 <phaugen@gmail.com>
2018-06-12 11:18:44 -0500

    skeleton set up

---
+++ docs/hazeltine/chap_02/equations.js
@@ -0,0 +1,794 @@
+var equations = [
+
+  {
+    equation:`NA`,
+    id:"02.001",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.002",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.003",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.004",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.005",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.006",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.007",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.008",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.009",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.010",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.011",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.012",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.013",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.014",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.015",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.016",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.017",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.018",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.019",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.020",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.021",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.022",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.023",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.024",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.025",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.026",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.027",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.028",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.029",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.030",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.031",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.032",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.033",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.034",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.035",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.036",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.037",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.038",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.039",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.040",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.041",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.042",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.043",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.044",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.045",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.046",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.047",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.048",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.049",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.050",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.051",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.052",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.053",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.054",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.055",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.056",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.057",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.058",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.059",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.060",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.061",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.062",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.063",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.064",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.065",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.066",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.067",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.068",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.069",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.070",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.071",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.072",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.073",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.074",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.075",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.076",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.077",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.078",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.079",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.080",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.081",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.082",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.083",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.084",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.085",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.086",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.087",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.088",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.089",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.090",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.091",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.092",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.093",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.094",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.095",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.096",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.097",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.098",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"02.099",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+]
