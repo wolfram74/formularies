49d904168e382fb99594cbb1233857619bf5187c
wolfram74 <phaugen@gmail.com>
2018-06-18 15:49:15 -0500

    some more of chapt 2

--- docs/hazeltine/chap_02/equations.js
+++ docs/hazeltine/chap_02/equations.js
@@ -285,194 +285,287 @@ var equations = [
   },
 
   {
-    equation:`NA`,
+    equation:`S_{,i}\\equiv (\\nabla S)_i = \\frac{\\partial S}{\\partial\\xi^i}`,
     id:"02.026",
     bookPage:"25",
-    label:"NA",
+    label:"Covariant derivative a scalar",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`
+    \\frac{\\partial}{\\partial \\xi^j} \\vec A =
+    \\frac{\\partial A_i}{\\partial \\xi^j} \\nabla \\xi ^i +
+    A_i \\frac{\\partial}{\\partial \\xi^j} \\nabla \\xi ^i
+    \\\\
+    \\frac{\\partial \\vec A}{\\partial \\xi^j} = A_{i,j}\\nabla \\xi ^i
+    `,
     id:"02.027",
     bookPage:"25",
-    label:"NA",
+    label:"Directional covariant derivative",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`
+    A_{i,j} = (\\partial_{\\xi^j} \\vec A)_i=
+    \\partial_{\\xi^j} A_i + \\Gamma_{ji}^k A_k
+    `,
     id:"02.028",
     bookPage:"25",
-    label:"NA",
+    label:"another form of the directional derivative",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`\\Gamma_{ji}^k\\equiv (\\partial_{\\xi^j} \\nabla \\xi^k)_i`,
     id:"02.029",
     bookPage:"25",
-    label:"NA",
-    description:"NA",
+    label:"definition of the christoffel symbol",
+    description:"also known as affine connection",
   },
 
   {
-    equation:`NA`,
+    equation:`
+    A^i_{,j}\\equiv
+    (\\partial_{\\xi^j} \\vec A)^i=
+    \\partial_{\\xi^j} A_i - \\Gamma_{jk}^i A^k
+    `,
     id:"02.030",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"26",
+    label:"the contravariant components of the directional derivative",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`
+    \\Gamma^i_{jk} =
+    -\\frac{1}{2} g^{im}(
+      \\partial_{\\xi^j} g_{mi}+
+      \\partial_{\\xi^i} g_{mj}-
+      \\partial_{\\xi^m} g_{jk}
+      )
+    `,
     id:"02.031",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"26",
+    label:"expressing the christoffel symbol in terms of the metric",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`
+    \\nabla \\cdot \\vec A =
+    \\nabla \\xi^m \\cdot \\partial_{\\xi^m}(
+      \\frac{\\epsilon_{ijk}}{2\\jmath}
+      A^i \\nabla \\xi^j
+      \\times \\nabla \\xi^k
+      )
+    =
+    \\jmath \\partial_{\\xi^i}(\\frac{1}{\\jmath}A^i)
+    `,
     id:"02.032",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"26",
+    label:"generic divergence",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`
+    B^i =
+    \\nabla\\xi^i \\cdot \\nabla\\xi^j \\times(
+      \\partial_{\\xi^j}\\nabla\\xi^m A_m
+      ) = \\jmath \\epsilon_{ijk}\\partial_{\\xi ^j}A_k
+    `,
     id:"02.033",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"26",
+    label:"generic curl",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`
+    S[x]=\\int_{t_0}^{t_1}L(
+      \\vec x(t),\\vec {\\dot{x}}(t),t
+      ) dt
+    `,
     id:"02.034",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"27",
+    label:"Standard action integral",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`0 = -\\frac{d}{dt}\\partial_{\\dot x^i} L + \\partial_{x^i} L`,
     id:"02.035",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"27",
+    label:"Euler lagrange equations",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`H(\\vec p, \\vec x) = \\vec p \\cdot \\vec {\\dot x}-
+    L(
+      \\vec x,\\vec {\\dot{x}},t
+      )`,
     id:"02.036",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"27",
+    label:"means of calculating hamiltonian",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`\\vec p = \\partial_{\\vec{\\dot x}} L`,
     id:"02.037",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"27",
+    label:"conjugate momentum",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`
+    S[\\vec p,\\vec x]=\\int_{t_0}^{t_1} (
+      \\vec p \\cdot \\vec {\\dot x}-H(\\vec p, \\vec x,t)
+      ) dt
+    `,
     id:"02.038",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"28",
+    label:"Phase space action integral",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`
+    \\dot p^i = - \\partial_{x^i} H
+    \\\\
+    \\dot x^i = \\partial_{p^i} H
+    `,
     id:"02.039",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"28",
+    label:"Hamilton's equations of motion",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`S[\\vec z] =
+    \\int_{t_0}^{t_1} (
+      \\vec A(\\vec z) \\cdot \\vec {\\dot z}-H(\\vec z,t)
+      ) dt
+    `,
     id:"02.040",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"28",
+    label:"Action integral in general coordinates",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`\\omega_{ij}\\dot z^j = \\partial_{z^i}H`,
     id:"02.041",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"29",
+    label:"hamiltons equations of motion in general coordinates",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`\\omega_{ij}=\\partial_{z_i}A_j-\\partial_{z_j}A_i`,
     id:"02.042",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"29",
+    label:"defining the weighting in general equations of motions",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`
+    \\omega
+    \\begin{bmatrix}
+      \\vec {\\dot p}\\\\ \\vec {\\dot x}
+    \\end{bmatrix}
+    =
+    \\begin{bmatrix}
+      0 & \\hat I
+      \\\\
+      -\\hat I & 0
+    \\end{bmatrix}
+
+    \\begin{bmatrix}
+      \\vec {\\dot p}\\\\ \\vec {\\dot x}
+    \\end{bmatrix}
+    `,
     id:"02.043",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"29",
+    label:"the symplectic form in simplest form",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`
+    L(
+      \\vec x,\\vec {\\dot{x}},t
+      ) = \\frac{m_s}{2}|\\dot x|^2-e_s \\Phi(\\vec x, t)
+    +\\frac{e_s}{c} \\vec{\\dot x} \\cdot \\vec A(\\vec x, t)
+    `,
     id:"02.044",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"29",
+    label:"non-relativisitc E&M lagrangian",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`
+      m\\frac{d^2\\vec x}{dt^2}
+      = e\\vec E
+      + \\frac{e}{c}\\frac{d\\vec x}{dt} \\times \\vec B
+    `,
     id:"02.045",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"31",
+    label:"the lorentz Force law",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`
+    E_i =
+    (-\\nabla \\Phi - \\frac{1}{c}\\partial_t A)_i=
+    -\\partial_{x^i} \\Phi - \\frac{1}{c}\\partial_t A_i
+    \\\\
+    \\epsilon_{ijk} B^i
+    = \\epsilon_{ijk}(\\nabla \\times \\vec A)^i
+    = \\jmath (\\partial_{x^j} A_k - \\partial_{x^k} A_j)
+    `,
     id:"02.046",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"31",
+    label:"Defining fields in terms of potentials",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`\\vec p = m \\vec v +\\frac{e}{c}\\vec A`,
     id:"02.047",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"31",
+    label:"Canonical momentum for charged particles",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`H=\\frac{1}{2m}|\\vec p - \\frac{e}{c} \\vec A|^2+e\\Phi`,
     id:"02.048",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"31",
+    label:"E&M hamiltonian",
     description:"NA",
   },
 
   {
-    equation:`NA`,
+    equation:`
+    \\nabla \\cdot \\vec E = 4\\pi \\rho_c
+    \\\\
+    \\nabla \\times \\vec B - \\frac{1}{c} \\partial_t \\vec E = \\frac{4\\pi}{c} \\vec J
+    \\\\
+    \\nabla \\times \\vec E  + \\frac{1}{c} \\partial_t \\vec B = 0
+    \\\\
+    \\nabla\\cdot \\vec B = 0
+    `,
     id:"02.049",
-    bookPage:"NA",
-    label:"NA",
+    bookPage:"31",
+    label:"Maxwell's equations",
     description:"NA",
   },
 
