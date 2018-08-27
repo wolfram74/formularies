9137c0cd2dcf3a990f3de0e6ab8b9ad0db341774
wolfram74 <phaugen@gmail.com>
2018-08-27 16:42:17 -0600

    templated greiner chap 1

---
+++ docs/greiner_stat_mech/chap_01/peter_equations.js
@@ -0,0 +1,138 @@
+var peter_equations = [
+
+  {
+    equation:`NA`,
+    id:"01.046",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"01.047",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"01.048",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"01.049",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"01.050",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"01.051",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"01.052",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"01.053",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"01.054",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"01.055",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"01.056",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"01.057",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"01.058",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"01.059",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"01.060",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"01.061",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+
+  {
+    equation:`NA`,
+    id:"01.062",
+    bookPage:"NA",
+    label:"NA",
+    description:"NA",
+  },
+]
---
+++ docs/greiner_stat_mech/chap_01/stitcher.js
@@ -0,0 +1,6 @@
+
+var equations = []
+equations = equations.concat(jacob_equations)
+equations = equations.concat(eric_equations)
+equations = equations.concat(bret_equations)
+equations = equations.concat(peter_equations)
